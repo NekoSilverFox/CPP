f0cb45dd18e3b607cab2fd6edbdfba520144bef2
冰糖雪狸 <weidufox@gmail.com>
2020-03-05 19:25:23 +0300

    补全 149 - 二叉树

--- 149 - 二叉树/149 - Tree.cpp
+++ 149 - 二叉树/149 - Tree.cpp
@@ -1,43 +1,43 @@
-//#include"LinkStack.h"
-//#include"Tree.h"
-//
-//
-//template<typename T>
-//void BinaryTree<T>::Foreach_Tree_DLR(Node* root)
-//{
-//}
-//
-//template<typename T>
-//void BinaryTree<T>::Foreach_Tree_LDR(Node* root)
-//{
-//	}
-//
-//template<typename T>
-//void BinaryTree<T>::Foreach_Tree_LRD(Node* root)
-//{
-//	}
-//
-//template<typename T>
-//int BinaryTree<T>::CaculateLeafNum(Node* root)
-//{
-//	}
-//
-//template<typename T>
-//int BinaryTree<T>::CaculateTreeDepth(Node* root)
-//{
-//	}
-//
-//template<typename T>
-//(BinaryTree::Node)* BinaryTree<T>::CopyBinaryTree(Node* root)
-//{
-//}
-//
-//template<typename T>
-//void BinaryTree<T>::FreeSpaceBinaryTree(Node* root)
-//{
-//	}
-//
-//template<typename T>
-//Node* BinaryTree<T>::CreatBinaryTreeByMethodPound()
-//{
-//	}
+#include"LinkStack.h"
+#include"Tree.h"
+
+void Tree()
+{
+	//BinaryTree<char>* t1 = new BinaryTree('A', nullptr, nullptr, nullptr);
+	//BinaryTree<char>* t2 = new BinaryTree('B', nullptr, nullptr, nullptr);
+	//BinaryTree<char>* t3 = new BinaryTree('F', nullptr, nullptr, nullptr);
+	//BinaryTree<char>* t4 = new BinaryTree('C', nullptr, nullptr, nullptr);
+	//BinaryTree<char>* t5 = new BinaryTree('D', nullptr, nullptr, nullptr);
+	//BinaryTree<char>* t6 = new BinaryTree('G', nullptr, nullptr, nullptr);
+	//BinaryTree<char>* t7 = new BinaryTree('E', nullptr, nullptr, nullptr);
+	//BinaryTree<char>* t8 = new BinaryTree('H', nullptr, nullptr, nullptr);
+	//BinaryTree<char>* t9 = new BinaryTree('I', nullptr, nullptr, nullptr);
+
+	/*BinaryTree<char> t1('A');
+	BinaryTree<char> t2('B');
+	BinaryTree<char> t3('F');
+	BinaryTree<char> t4('C');
+	BinaryTree<char> t5('D');
+	BinaryTree<char> t6('G');
+	BinaryTree<char> t7('E');
+	BinaryTree<char> t8('H');
+	BinaryTree<char> t9('I');
+
+	t1.root_->left_ = t2.root_;
+	t1.root_->right_ = t3.root_;
+	t2.root_->left_ = t4.root_;
+	t2.root_->right_ = t5.root_;
+	t3.root_->right_ = t6.root_;
+	t5.root_->left_ = t7.root_;
+	t6.root_->left_ = t8.root_;
+	t6.root_->right_ = t9.root_;
+
+	t1.Foreach_Tree_DLR(t1.root_);*/
+
+	BinaryTree<char> charTree;
+	charTree.insertTree();
+	charTree.printTree("ALL");
+	charTree.getDepth();
+	charTree.getLeafnum();
+	charTree.deleteTree();
+}
--- 149 - 二叉树/Tree.h
+++ 149 - 二叉树/Tree.h
@@ -5,13 +5,15 @@
 #include <string>
 using namespace std;
 
-
 template<typename T>
 class BinaryTree
 {
+	// ---------------------------- Node ----------------------------
 private:
 	struct Node
 	{
+		Node() {}
+		Node(T key) : key_(key), left_(nullptr), right_(nullptr), p_(nullptr) {}
 		Node(T key, Node* left, Node* right, Node* p) : key_(key), left_(left), right_(right), p_(p) {}
 
 		T key_;
@@ -20,13 +22,101 @@ private:
 		Node* p_;
 	};
 
+private:
+	Node* root_;
+	// -----------------------------------------------------------------
+
 public:
-	BinaryTree(NT key, Node* left, Node* right, Node* p)
+	BinaryTree()
+	{
+		root_ = new Node;
+		root_->key_ = nullptr;
+		root_->left_ = nullptr;
+		root_->right_ = nullptr;
+		root_->p_ = nullptr;
+	}
+	BinaryTree(T key)
+	{
+		root_ = new Node<T>(key);
+	}
+	BinaryTree(T key, Node* left, Node* right, Node* p)
+	{
+		root_ = new Node<T>(key, left, right, p);
+	}
+
+	void insertTree()
+	{
+		root_ = CreatBinaryTreeByMethodPound();
+	}
+
+	void printTree(string method) const 
+	{
+		if ("DLR" == method) { Foreach_Tree_DLR(this->root_); }
+		else if ("LDR" == method) { Foreach_Tree_LDR(this->root_); }
+		else if ("LRD" == method) { Foreach_Tree_LRD(this->root_); }
+		else if ("ALL" == method)
+		{
+			cout << "DLR : " << Foreach_Tree_DLR(this->root_) << endl;
+			cout << "LDR : " << Foreach_Tree_DLR(this->root_) << endl;
+			cout << "LRD : " << Foreach_Tree_DLR(this->root_) << endl;
+		}
+		else
+		{
+			cerr << "----- Unknown command -----" << endl;
+			cout << "， DLR" << endl
+					<< "， LDR" << endl
+					<< "， LRD" << endl
+					<< "， ALL" << endl;
+		}
+	}
+
+	int getLeafnum() const
+	{
+		return CaculateLeafNum(this->root_);
+	}
+
+	int getDepth() const
+	{
+		return CaculateTreeDepth(this->root_);
+	}
+
+	Node* copyTree() const 
+	{
+		return CopyBinaryTree(this->root_);
+	}
+
+	void deleteTree() const
+	{
+		FreeSpaceBinaryTree(root_);
+	}
+
+	// ---------------------------- --- ----------------------------
+private:
+	Node* CreatBinaryTreeByMethodPound()
+	{
+		Node* node = new Node;
+
+		T key;
+		cout << ">> "; cin >> key;
+
+		if (key == '#')
+		{
+			node = nullptr;
+		}
+		else
+		{
+			node->key_ = key;
+			node->left_ = CreatBinaryTreeByMethodPound();
+			node->right_ = CreatBinaryTreeByMethodPound();
+		}
+		return node;
+	}
+
 	void Foreach_Tree_DLR(Node* root)
 	{
 		if (root == nullptr) return;
 
-		cout << root->key_;
+		cout << root->key_ << " ";
 		Foreach_Tree_DLR(root->left_);
 		Foreach_Tree_DLR(root->right_);
 	}
@@ -49,7 +139,6 @@ public:
 		cout << root->key_;
 	}
 
-	// The number of leaf
 	int CaculateLeafNum(Node* root)
 	{
 		static int leaf_num = 0;
@@ -65,9 +154,10 @@ public:
 		int depth = 0;
 		int left_depth = CaculateTreeDepth(root->left_);
 		int right_depth = CaculateTreeDepth(root->right_);
-		depth = left_depth > right_depth ? ++left_depth, ++right_depth;
+		depth = left_depth > right_depth ? ++left_depth : ++right_depth;
 		return depth;
 	}
+
 	Node* CopyBinaryTree(Node* root)
 	{
 		if (root == nullptr) return nullptr;
@@ -78,7 +168,7 @@ public:
 		newNode->key_ = root->key_;
 		newNode->left_ = left_child;
 		newNode->right = right_child;
-	}
+	} 
 
 	void FreeSpaceBinaryTree(Node* root)
 	{
@@ -88,12 +178,5 @@ public:
 		delete root;
 	}
 
-	Node* CreatBinaryTreeByMethodPound()
-	{
-
-	}
-
-private:
-	Node* root_;
 };
 #endif
