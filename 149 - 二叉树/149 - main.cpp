dd4b11ef1b799a43586446649d731e9efa5c5389
冰糖雪狸 <weidufox@gmail.com>
2020-03-07 00:58:49 +0300

    更新 149 - 二叉树，增加 Iterative search by key

--- 149 - 二叉树/149 - main.cpp
+++ 149 - 二叉树/149 - main.cpp
@@ -1,8 +1,28 @@
 #include"LinkStack.h"
 #include"Tree.h"
 
+void FunCreatBinaryTree()
+{
+	BinaryTree<char> charTree;
+	charTree.insertTree();
+	cout << endl << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-" << endl;
+	charTree.printTree("ALL");
+	cout << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-" << endl;
+	cout << "Depth : " << charTree.getDepth() << endl;
+	cout << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-" << endl;
+	cout << "Leaf number : " << charTree.getLeafNum() << endl;
+	cout << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-" << endl;
+	cout << "Element number : " << charTree.getElementNum() << endl;
+	cout << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-" << endl;
+	cout << "Iterative search by element>>>>> " << endl; 
+	if (charTree.iterativeSearch()) cout << "True" << endl;
+	else cout << "False" << endl;
+	cout << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-" << endl;
+	charTree.deleteTree();
+}
+
 
 int main()
 {
-
+	FunCreatBinaryTree();
 }
--- 149 - 二叉树/Tree.h
+++ 149 - 二叉树/Tree.h
@@ -3,6 +3,7 @@
 #define TREE_H
 #include <iostream>
 #include <string>
+#include<Windows.h>
 using namespace std;
 
 template<typename T>
@@ -22,6 +23,13 @@ private:
 		Node* p_;
 	};
 
+	struct BinTreeStackNode
+	{
+		LinkNode node;
+		Node* root;
+		bool flag;
+	};
+
 private:
 	Node* root_;
 	// -----------------------------------------------------------------
@@ -30,7 +38,7 @@ public:
 	BinaryTree()
 	{
 		root_ = new Node;
-		root_->key_ = nullptr;
+		root_->key_ = NULL;
 		root_->left_ = nullptr;
 		root_->right_ = nullptr;
 		root_->p_ = nullptr;
@@ -49,16 +57,16 @@ public:
 		root_ = CreatBinaryTreeByMethodPound();
 	}
 
-	void printTree(string method) const 
+	void printTree(string method) 
 	{
 		if ("DLR" == method) { Foreach_Tree_DLR(this->root_); }
 		else if ("LDR" == method) { Foreach_Tree_LDR(this->root_); }
 		else if ("LRD" == method) { Foreach_Tree_LRD(this->root_); }
 		else if ("ALL" == method)
 		{
-			cout << "DLR : " << Foreach_Tree_DLR(this->root_) << endl;
-			cout << "LDR : " << Foreach_Tree_DLR(this->root_) << endl;
-			cout << "LRD : " << Foreach_Tree_DLR(this->root_) << endl;
+			cout << "DLR : "; Foreach_Tree_DLR(this->root_); cout << endl;
+			cout << "LDR : "; Foreach_Tree_LDR(this->root_); cout << endl;
+			cout << "LRD : "; Foreach_Tree_LRD(this->root_); cout << endl;
 		}
 		else
 		{
@@ -70,12 +78,17 @@ public:
 		}
 	}
 
-	int getLeafnum() const
+	int getElementNum()
+	{
+		return CaculateElementNum(this->root_);
+	}
+
+	int getLeafNum()
 	{
 		return CaculateLeafNum(this->root_);
 	}
 
-	int getDepth() const
+	int getDepth()
 	{
 		return CaculateTreeDepth(this->root_);
 	}
@@ -85,12 +98,17 @@ public:
 		return CopyBinaryTree(this->root_);
 	}
 
-	void deleteTree() const
+	bool iterativeSearch()
 	{
-		FreeSpaceBinaryTree(root_);
+		return (IterativeSearchNode(this->root_) != nullptr);
 	}
 
-	// ---------------------------- --- ----------------------------
+	void deleteTree()
+	{
+		FreeSpaceBinaryTree(this->root_);
+	}
+
+	// -----------------------------------------------------------
 private:
 	Node* CreatBinaryTreeByMethodPound()
 	{
@@ -114,7 +132,7 @@ private:
 
 	void Foreach_Tree_DLR(Node* root)
 	{
-		if (root == nullptr) return;
+		if (nullptr == root) return;
 
 		cout << root->key_ << " ";
 		Foreach_Tree_DLR(root->left_);
@@ -123,34 +141,53 @@ private:
 
 	void Foreach_Tree_LDR(Node* root)
 	{
-		if (root == nullptr) return;
+		if (nullptr == root) return;
 
 		Foreach_Tree_LDR(root->left_);
-		cout << root->key_;
+		cout << root->key_ << " ";
 		Foreach_Tree_LDR(root->right_);
 	}
 
 	void Foreach_Tree_LRD(Node* root)
 	{
-		if (root == nullptr) return;
+		if (nullptr == root) return;
 
 		Foreach_Tree_LRD(root->left_);
 		Foreach_Tree_LRD(root->right_);
-		cout << root->key_;
+		cout << root->key_ << " ";
+	}
+
+	// 创建栈中的结点 - Creat element in stack
+	BinTreeStackNode* CreatBinTreeStackNode(Node* node, bool flag)
+	{
+		BinTreeStackNode* newNode = new BinTreeStackNode;
+		newNode->root = node;
+		newNode->flag = false;
+		return newNode;
+	}
+
+	int CaculateElementNum(Node* root)
+	{
+		if (nullptr == root) return 0;
+		static int element_num = 0;
+		CaculateElementNum(root->left_); element_num++;
+		CaculateElementNum(root->right_);
+		return element_num;
 	}
 
 	int CaculateLeafNum(Node* root)
 	{
 		static int leaf_num = 0;
-		if (root == nullptr) return;
+		if (nullptr == root) return 0;
 		if (root->left_ == nullptr && root->right_ == nullptr) leaf_num++;
 		CaculateLeafNum(root->left_); // number of the left leaf
 		CaculateLeafNum(root->right_); // number of the right leaf
+		return leaf_num;
 	}
 
 	int CaculateTreeDepth(Node* root)
 	{
-		if (root == nullptr) return 0;
+		if (nullptr == root) return 0;
 		int depth = 0;
 		int left_depth = CaculateTreeDepth(root->left_);
 		int right_depth = CaculateTreeDepth(root->right_);
@@ -158,9 +195,93 @@ private:
 		return depth;
 	}
 
+	Node* IterativeSearchNode(Node* root)
+	{
+		// 创建栈
+		LinkStack* stack = InIt_LinkStack();
+		Node* search_node = new Node;
+		search_node = nullptr;
+		//int counter = 0;
+		//int place_key[50];
+		//int i = 0;
+		T search_key;
+
+		cout << "Which key need to search : "; cin >> search_key;
+
+		// 将根结点放到栈中
+		Push_LinkStack(stack, (LinkNode*)CreatBinTreeStackNode(root, false));
+
+		while (Size_LinkStack(stack) > 0)
+		{
+			// 先弹出栈顶元素
+			BinTreeStackNode* node = (BinTreeStackNode*)Top_LinkStack(stack);
+			Pop_LinkStack(stack);
+
+			// 判断，如果为 nullptr 直接重新循环
+			if (node->root == nullptr) continue;
+
+			if (node->flag == true)
+			{
+				if (search_key == node->root->key_)
+				{
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+					cout << node->root->key_ << " ";
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+				}
+				else
+				{
+					cout << node->root->key_ << " ";
+				}
+				//++counter;
+				//if (node->root->key_ == search_key)
+				//{
+				//	if (0 == i)
+				//	{
+				//		place_key[i] = counter - 1;
+				//		++i;
+				//	}
+				//	if (i > 0)
+				//	{
+				//		place_key[i] = counter - place_key[i - 1];
+				//		++i;
+				//	}	
+				//	search_node = node->root;
+				//}
+				search_node = node->root;
+			}
+			else {
+				Push_LinkStack(stack, (LinkNode*)CreatBinTreeStackNode(node->root->right_, false));
+				Push_LinkStack(stack, (LinkNode*)CreatBinTreeStackNode(node->root->left_, false));
+				node->flag = true;
+				Push_LinkStack(stack, (LinkNode*)node);
+			}
+		}
+		cout << endl;
+		//for (int j = 0; j < i; ++j)
+		//{
+		//	for (int space_ = 0; space_ < place_key[j]; ++space_) cout << " ";
+		//	cout << "A";
+		//}
+		return search_node;
+	}
+
+	T OutputDataElement(Node* root) // xxx
+	{
+		if (nullptr == root) return 0;
+		unsigned int no_element;
+		cout << "Element number :  "; cin >> no_element;
+		if (no_element > CaculateElementNum(root))
+		{
+			cerr << "Don't have data!" << endl
+				<< "The element number should less than " << CaculateElementNum() << " !" << endl;
+			return 0;
+		}
+
+	}
+
 	Node* CopyBinaryTree(Node* root)
 	{
-		if (root == nullptr) return nullptr;
+		if (nullptr == root) return nullptr;
 		Node* left_child = CopyBinaryTree(root->left_);
 		Node* right_child = CopyBinaryTree(root->right_);
 
@@ -172,7 +293,7 @@ private:
 
 	void FreeSpaceBinaryTree(Node* root)
 	{
-		if (root == nullptr) return;
+		if (nullptr == root) return;
 		FreeSpaceBinaryTree(root->left_);
 		FreeSpaceBinaryTree(root->right_);
 		delete root;
