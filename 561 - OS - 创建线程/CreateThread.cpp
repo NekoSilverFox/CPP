652b4435b2b39cac47eabeb32a557720876240c1
foxthere <weidufox@gmail.com>
2021-02-23 23:04:17 +0300

    增加 创建线程的相关注释

--- 561 - OS - 创建线程/CreateThread.cpp
+++ 561 - OS - 创建线程/CreateThread.cpp
@@ -5,6 +5,8 @@
 
 // нить / поток - 线程
 
+
+// LPVOID 是一个没有类型的指针
 DWORD WINAPI ThreadProc(LPVOID lpParameter)
 {
 	int param;
@@ -16,13 +18,16 @@ DWORD WINAPI ThreadProc(LPVOID lpParameter)
 	{
 		printf("ThreadProc=%d in while!\n", param);
 
-		Sleep(100);
+		Sleep(100); // ms - миллисекунда
 	}
 
 }
 
 int main(int argc, char* argv[])
 {
+	// HANDLE 在程序设计中，句柄（handle）是Windows操作系统用来标识被应用程序所建立或使用的对象的整数。
+	// 其本质相当于带有引用计数的智能指针。当一个应用程序要引用其他系统（如数据库、操作系统）所管理的内存块或对象时，可以使用句柄。
+
 	HANDLE mHandle[2];
 	DWORD mArg[2], mId[2];
 	int i;
@@ -37,12 +42,12 @@ int main(int argc, char* argv[])
 
 	Sleep(1000);
 
-	/*	while(TRUE)
-		{
-			printf("Main in while!\n");
+	//while(TRUE)
+	//	{
+	//		printf("Main in while!\n");
 
-			Sleep(100);
-		}*/
+	//		Sleep(100);
+	//	}
 	return 0;
 
 }
